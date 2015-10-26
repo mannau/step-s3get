665d89890ae577ae0f5c49c0679894d5b0839220
mario <mario.annau@gmail.com>
2015-10-26 09:44:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# s3sync
+
+Forked from wercker/s3cmd and adjusted for s3cmd get
+
+It is recommended that you use application and deployment variables in wercker, so you don't include any private keys in your code.
+
+# Options
+
+* `key-id` (required) The Amazon Access key that will be used for authorization.
+* `key-secret` (required) The Amazon Access secret that will be used for authorization.
+* `bucket-url` (required) The url of the bucket to sync to, like: `s3://wercker.com`, where `wercker.com` is the bucket name.
+* `source-dir` (optional, default: `./`) The directory to sync to the remote bucket.
+* `delete-removed` (optional, default: `true`) Add `--delete-remove` flag if this is `true`.
+* `opts` (optional, default: `--acl-public`) Arbitrary options provided to s3cmd. See `s3cmd --help` for more.
+
+# Example
+
+```
+deploy:
+    steps:
+        - s3get:
+            key-id: $KEY
+            key-secret: $SECRET
+            bucket-url: $BUCKET
+            source-dir: $SOURCE
+```
+
+# License
+
+The MIT License (MIT)
+
+# Changelog
+
+## 1.0
+
+- Initial Release
+
---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set_auth() {
+  local s3cnf="$HOME/.s3cfg"
+
+  if [ -e "$s3cnf" ]; then
+    warn '.s3cfg file already exists in home directory and will be overwritten'
+  fi
+
+  echo '[default]' > "$s3cnf"
+  echo "access_key=$WERCKER_S3SYNC_KEY_ID" >> "$s3cnf"
+  echo "secret_key=$WERCKER_S3SYNC_KEY_SECRET" >> "$s3cnf"
+
+  debug "generated .s3cfg for key $WERCKER_S3SYNC_KEY_ID"
+}
+
+main() {
+  set_auth
+
+  info 'starting s3 synchronisation'
+
+  if [ ! -n "$WERCKER_S3SYNC_KEY_ID" ]; then
+    fail 'missing or empty option key_id, please check wercker.yml'
+  fi
+
+  if [ ! -n "$WERCKER_S3SYNC_KEY_SECRET" ]; then
+    fail 'missing or empty option key_secret, please check wercker.yml'
+  fi
+
+  if [ ! -n "$WERCKER_S3SYNC_BUCKET_URL" ]; then
+    fail 'missing or empty option bucket_url, please check wercker.yml'
+  fi
+
+  if [ ! -n "$WERCKER_S3SYNC_OPTS" ]; then
+    export WERCKER_S3SYNC_OPTS="--acl-public"
+  fi
+
+  source_dir="$WERCKER_ROOT/$WERCKER_S3SYNC_SOURCE_DIR"
+  if cd "$source_dir";
+  then
+      debug "changed directory $source_dir, content is: $(ls -l)"
+  else
+      fail "unable to change directory to $source_dir"
+  fi
+
+  set +e
+  local SYNC="$WERCKER_STEP_ROOT/s3cmd get $WERCKER_S3SYNC_OPTS --verbose ./ $WERCKER_S3SYNC_BUCKET_URL"
+  debug "$SYNC"
+  local sync_output=$($SYNC)
+
+  if [[ $? -ne 0 ]];then
+      echo "$sync_output"
+      fail 's3cmd failed';
+  else
+      echo "$sync_output"
+      success 'finished s3 synchronisation';
+  fi
+  set -e
+}
+
+main
